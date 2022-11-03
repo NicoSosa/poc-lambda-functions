a0bdc8e38a3ecb8fa89618f55f9c384425460ac0
NicoSosa <nico.js69@gmail.com>
2022-11-03 00:11:54 -0300

    added try catch method at index.js

--- index.js
+++ index.js
@@ -7,36 +7,40 @@ exports.handler = async (event, context, callback) => {
     let response;
 
     console.log('Request Event', event);
-    switch (true) {
-        // TASK-PACKAGE
-        case event.resource === paths.tasksPackagePath:
-            response = await resources.taskPackageResource.resourceMethod(event)
-            break;
-
-        case event.resource === paths.tasksPackageIdPath:
-            response = await resources.taskPackageIdResource.resourceMethod(event)
-            break;
-
-        // TEMPLATE
-        case event.resource === paths.templatePath:
-            response = await resources.templateResource.resourceMethod(event)
-            break;
-
-        case event.resource === paths.templateIdPath:
-            response = await resources.templateIdResource.resourceMethod(event)
-            break;  
-        
-        // WORKSPACE
-        case event.resource === paths.workspacePath:
-            response = await resources.workspaceResource.resourceMethod(event)
-            break;
-
-        case event.resource === paths.workspaceIdPath:
-            response = await resources.workspaceIdResource.resourceMethod(event)
-            break;
-
-        default:
-            response = util.buildResponse(404, '404 - Not Found');
+    try {
+        switch (true) {
+            // TASK-PACKAGE
+            case event.resource === paths.tasksPackagePath:
+                response = await resources.taskPackageResource.resourceMethod(event)
+                break;
+    
+            case event.resource === paths.tasksPackageIdPath:
+                response = await resources.taskPackageIdResource.resourceMethod(event)
+                break;
+    
+            // TEMPLATE
+                case event.resource === paths.templatePath:
+                response = await resources.templateResource.resourceMethod(event)
+                break;
+    
+            case event.resource === paths.templateIdPath:
+                response = await resources.templateIdResource.resourceMethod(event)
+                break;  
+            
+            // WORKSPACE
+            case event.resource === paths.workspacePath:
+                response = await resources.workspaceResource.resourceMethod(event)
+                break;
+    
+            case event.resource === paths.workspaceIdPath:
+                response = await resources.workspaceIdResource.resourceMethod(event)
+                break;
+    
+            default:
+                response = util.buildResponse(404, '404 - Not Found');
+        }
+    } catch (e) {
+        response = util.buildResponse(500, e);
     }
     return response;
 };
